0f7dc68f7fbed0839844b0a1c2638847f924e1db
dcattaneo <dcattaneo@gmail.com>
2025-01-06 22:14:15 +0100

    fixed the color switch position

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,11 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} alt="logo" boxSize="50px" />
-      <Text fontSize="xl" fontWeight="bold">
-        Nav Bar
-      </Text>
       <ColorModeSwitch />
     </HStack>
   );
