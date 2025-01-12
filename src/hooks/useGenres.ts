a6ea7c0795494328527355dacce4b9a1f2a33fe6
dcattaneo <dcattaneo@gmail.com>
2025-01-12 10:28:58 +0100

    genres static data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,15 @@
-import useData from "./useData";
+//import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
+//fetch data form the server
+//const useGenres = () => useData<Genre>("/genres")
 
-const useGenres = () => useData<Genre>("/genres")
+//static data included in the project
+const useGenres = () => ({data: genres, isLoading: false, error: null});
 
 export default useGenres;
