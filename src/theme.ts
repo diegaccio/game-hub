40ae7d8654faa91d739c65b83b6f7354ddf6ba1e
dcattaneo <dcattaneo@gmail.com>
2025-01-12 10:47:29 +0100

    customize the theme to get darker shades of grey

--- src/theme.ts
+++ src/theme.ts
@@ -10,6 +10,24 @@ const config: ThemeConfig = {
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ 
+  config, 
+   colors: {
+    //custom grey definition took from smart swatch generator
+    //https://smart-swatch.netlify.app/#121212
+    gray: {
+      50: '#f2f2f2',
+      100: '#d9d9d9',
+      200: '#bfbfbf',
+      300: '#a6a6a6',
+      400: '#8c8c8c',
+      500: '#737373',
+      600: '#595959',
+      700: '#404040',
+      800: '#262626',
+      900: '#0d0d0d',
+    }
+  }  
+});
 
 export default theme
