d53e997f7c4862c87ca95ce1ff2830b5be64cc54
diegaccio <dcattaneo@gmail.com>
2025-01-19 18:09:24 +0100

    fix a build error in useGenres and usePlatforms hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -21,7 +21,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
-    initialData: { count: platforms.length, results: platforms }, //this mast be a DataResponse <Genre> object
+    initialData: { count: platforms.length, results: platforms, next: null }, //this mast be a DataResponse <Genre> object
   });
 
 export default usePlatforms;
